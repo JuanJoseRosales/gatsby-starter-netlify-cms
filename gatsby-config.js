a2f98e4e677e8fb7126b9a6f57100752fb7a60ca
Juan Rosales <juanroga87@gmail.com>
2019-10-15 21:57:22 -0700

    Changing url, button and text. Also adding some logos

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,17 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Juan',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+          bucketName: 'www.juanjoserosales.com'
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
